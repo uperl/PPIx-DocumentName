56dbb7daecb83c4193de9d8a939b19a5462c3503
Kent Fredric <kentfredric@gmail.com>
2015-02-07 22:49:49 +1300

    Generation 0

--- maint/perlcritic.rc.gen.pl
+++ maint/perlcritic.rc.gen.pl
@@ -18,16 +18,19 @@ use Path::Tiny qw(path);
 my $bundle = create_bundle('Example::Author::KENTNL');
 $bundle->configure;
 
-my @stopwords = (qw());
+my @stopwords = (qw( PPI ));
 for my $wordlist (@stopwords) {
   $bundle->add_or_append_policy_field( 'Documentation::PodSpelling' => ( 'stop_words' => $wordlist ) );
 }
+$bundle->remove_policy('Subroutines::ProhibitSubroutinePrototypes');
+$bundle->remove_policy('Subroutines::RequireArgUnpacking');
+$bundle->remove_policy('Subroutines::RequireFinalReturn');
+
 
 #$bundle->add_or_append_policy_field(
 #  'Subroutines::ProhibitCallsToUndeclaredSubs' => ( 'exempt_subs' => 'String::Formatter::str_rf' ), );
 
 #$bundle->remove_policy('ErrorHandling::RequireUseOfExceptions');
-#$bundle->remove_policy('CodeLayout::RequireUseUTF8');
 #$bundle->remove_policy('ErrorHandling::RequireCarping');
 #$bundle->remove_policy('NamingConventions::Capitalization');
 
