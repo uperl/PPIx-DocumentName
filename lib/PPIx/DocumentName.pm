cc9babbb119c6a2097f501ca6b522811a72a4a9a
Kent Fredric <kentfredric@gmail.com>
2015-02-07 16:43:39 +1300

    Fix naming and add level zero files ( I should add most of these to my template >_> )

---
+++ lib/PPIx/DocumentName.pm
@@ -0,0 +1,13 @@
+use 5.006;    # our
+use strict;
+use warnings;
+
+package PPIx::DocumentName;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT: Utility to extract a name from a PPI Document
+
+# AUTHORITY
+
+1;
