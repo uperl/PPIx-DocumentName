1d2e756a27cd545992a5df504522fc973f47c5ee
Kent Fredric <kentfredric@gmail.com>
2015-02-08 19:47:50 +1300

    Build results of 2011a57 (on master)

--- lib/PPIx/DocumentName.pm
+++ lib/PPIx/DocumentName.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package PPIx::DocumentName;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Utility to extract a name from a PPI Document
 
@@ -135,7 +135,7 @@ PPIx::DocumentName - Utility to extract a name from a PPI Document
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 DESCRIPTION
 
@@ -169,7 +169,7 @@ The recommended approach is simply:
   my $docname = PPIx::DocumentName->extract( $ppi_document );
 
 This will first attempt to extract a name via the C<PODNAME: > comment notation,
-and then fallback to using a C<package Package::Name> statement.
+and then fall back to using a C<package Package::Name> statement.
 
 C<$ppi_document> is ideally a C<PPI::Document>, but will be auto-up-cast if it is
 any of the parameters C<< PPI::Document->new() >> understands.
@@ -200,11 +200,61 @@ Other things I could have called this
 
 =over 4
 
-=item * C<PPIx::PodName> - But it isn't, because it doesn't extract from C<POD>, only returns data that may be useful B<FOR> C<POD>
+=item * C<PPIx::PodName> - But it isn't, because it doesn't extract from C<POD>, only returns data that may be useful B<FOR>
+C<POD>
 
-=item * C<PPIx::ModuleName> - But it kinda isn't either, because its more generic than that and is tailored to extracting "a name" out of any PPI Document, and they're I<NOT> all modules.
+=item * C<PPIx::ModuleName> - But it kinda isn't either, because its more generic than that and is tailored to extracting
+"a name" out of any PPI Document, and they're I<NOT> all modules.
 
 =back
+
+=head1 SIMILAR MODULES
+
+Modules that are perceptibly similar to this ones tasks ( but are subtly different in important ways ) are as follows:
+
+=over 4
+
+=item * L<< C<Module::Metadata>|Module::Metadata >> - Module::Metadata does a bunch of things this module explicitly doesn't
+want or need to do, and it lacks a bunch of features this module needs.
+
+Module::Metadata is predominantly concerned with extracting I<ALL> name spaces and I<ALL> versions from a module for the
+purposes of indexing and indexing related tasks. This also means it has a notion of "hideable" name spaces with the purpose
+of hiding them from C<CPAN>.
+
+Due to being core as well, it is not able to use C<PPI> for its features, so the above concerns mean it is also mostly
+based on careful regex parsing, which can easily be false tripped on miscellaneous in document content.
+
+Whereas C<PPIx::DocumentName> only cares about the I<first> name of a given class, and it cares much more about nested
+strings being ignored intentionally. It also has a motive to show names I<even> for documents that won't be indexed
+( And C<Module::Metadata> has no short term plans on exposing hidden document names ).
+
+C<PPIx::DocumentName> also has special logic for the C<PODNAME: > declaration, and may eventually support other
+mechanisms for extracting a name from "a document", which will be not in C<Module::Metadata>'s collection of desired
+use-cases.
+
+=item * L<< C<Module::Extract::Namespaces>|Module::Extract::Namespaces >> - This is probably closer to
+C<PPIx::DocumentName>'s requirements, using C<PPI> to extract content.
+
+Most of C<Module::Extract::Namespaces>'s code seems to be glue for legacy versions of C<PPI> and the remaining
+code is for loading modules from C<@INC> ( Which we don't need ), or special casing IO ( Which is also not necessary,
+as this module assumes you're moderately acquainted with C<PPI> and can do IO yourself )
+
+C<Module::Extract::Namespaces> also obliterates document comments, which of course stands in the way of our auxiliary
+requirements re C<PODNAME: > declarations.
+
+It will also not be flexible enough to support other name extraction features we may eventually add.
+
+And like C<Module::Metadata>, it also focuses on extracting I<many> C<package> declarations where this module prefers
+to extract only the I<first>.
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+The bulk of this logic was extrapolated from L<< C<Pod::Weaver::Section::Name>|Pod::Weaver::Section::Name >>
+and a related role, L<< C<Pod::Weaver::Role::StringFromComment>|Pod::Weaver::Role::StringFromComment >>.
+
+Thanks to C<RJBS> for the initial implementation and C<DROLSKY> for some of the improvement patches.
 
 =head1 AUTHOR
 
