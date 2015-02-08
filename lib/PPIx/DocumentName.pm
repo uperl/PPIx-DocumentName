e7a9caee2aead85d525978b5596b1c5229f70836
Kent Fredric <kentfredric@gmail.com>
2015-02-09 01:29:36 +1300

    Build results of b71f66b (on master)

--- lib/PPIx/DocumentName.pm
+++ lib/PPIx/DocumentName.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package PPIx::DocumentName;
 
-our $VERSION = '0.001002';
+our $VERSION = '0.001003';
 
 # ABSTRACT: Utility to extract a name from a PPI Document
 
@@ -135,7 +135,7 @@ PPIx::DocumentName - Utility to extract a name from a PPI Document
 
 =head1 VERSION
 
-version 0.001002
+version 0.001003
 
 =head1 DESCRIPTION
 
